6bd7ae405b33382ecca87c019c5957be05db0186
W <granger.w35@gmail.com>
2022-08-30 09:22:47 +0200

    fix betty

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,19 +12,19 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
-	if(parent == NULL)
+	if (parent == NULL)
 		return(NULL);
 
 	new = binary_tree_node(parent, value);
-	if(new == NULL )
+	if (new == NULL)
 		return(NULL);
 
-	if(parent-> left != NULL)
+	if (parent->left != NULL)
 	{
-		new-> left = parent-> left ;
-		parent-> left -> parent = new;
+		new->left = parent->left ;
+		parent->left->parent = new;
 	}
-	parent-> left = new;
+	parent->left = new;
 
-	return(new);
+	return (new);
 }
