888b31340020ebee2700ddc5521c1f9d4920f126
Maykha27 <5144@holbertonschool.com>
2022-09-02 11:23:58 +0100

    7-binary_tree_inorder.c

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 /**
  * binary_tree_inorder - function that goes through a binary tree
- * using in-order traversal
+ * using in-order traversals
  * @tree: tree
  * @func: function
  * Return: void
